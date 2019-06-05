880f412a7871fb0edebc422be081d32194a8ffe3
Walker Hale IV <walker.hale.iv@gmail.com>
2019-06-05 13:30:44 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# conda\_demo\_bash
+
+Copyright 2019 Baylor College of Medicine
+
+questions@hgsc.bcm.edu
+
+Demo of a collection of bash scripts packaged by conda
---
+++ tests/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# The test is considered failed if this script exits nonzero.
+
+set -e  # bail if any direct command fails (not if or while conditions)
+
+true
